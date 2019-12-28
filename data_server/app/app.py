ba5f4ac6839ffd8430349570f2808b0294f8de70
MG <mtsgrinberg@gmail.com>
2019-12-28 04:53:33 -0300

    .

--- data_server/app/app.py
+++ data_server/app/app.py
@@ -14,7 +14,8 @@ s3 = boto3.client('s3', 'AKIAJZ46CC3EET5DNKAA', 'Q/f7tdaTWCe/fqSIQF7FFPBjwKSrlDx
 def get_data(to_s3=True): 
     out_name = f"gyani/{strftime('%Y-%m-%d %H-%M-%S')}_{request.referrer}.json"
     data = request.form['data']
-    
+    bucket_name = 'unket'
+
     if to_s3:
         resp = s3.put_object(Bucket=bucket_name,
                   Key=out_name,
